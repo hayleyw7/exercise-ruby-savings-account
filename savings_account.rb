b51f472303848ae8631bc00b1fa762dec0931f8b
Hayley Witherell <hayleywitherell@gmail.com>
2026-06-06 16:51:46 -0600

    Solve

--- savings_account.rb
+++ savings_account.rb
@@ -1,13 +1,29 @@
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    if balance < 0
+      3.213
+    elsif balance < 1000
+      0.5
+    elsif balance < 5000
+    1.621
+    else
+      2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    rate = interest_rate(balance) / 100
+    balance + (balance * rate)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    years = 0
+
+    while current_balance < desired_balance
+      current_balance = annual_balance_update(current_balance)
+      years += 1
+    end
+
+    years
   end
 end
